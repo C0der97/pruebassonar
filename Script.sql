cadc7b59d6c0c21489f269bae29e53a6269aa5ef
angel leon <38294654+C0der97@users.noreply.github.com>
2020-11-20 13:44:03 -0500

    Create Script.sql

---
+++ Script.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE dbo.procedimiento   
+    @LastName nvarchar(50),   
+    @FirstName nvarchar(50)   
+AS   
+  @cosa = 'CREATE TABLE sales.visits (
+    visit_id INT PRIMARY KEY IDENTITY (1, 1),
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    visited_at DATETIME,
+    phone VARCHAR(20),
+    store_id INT NOT NULL,
+    FOREIGN KEY (store_id) REFERENCES sales.stores (store_id)
+);';
+
+exec SP_EXECUTESQL @cosa;
+GO  
