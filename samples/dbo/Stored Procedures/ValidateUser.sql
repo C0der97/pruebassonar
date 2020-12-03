70f7e3ac8f744c6cab2ba51f510cdcabed28f2f4
c0der97 <bare-o-emilio15@hotmail.com>
2020-12-03 09:59:12 -0500

    ajustes

--- samples/dbo/Stored Procedures/ValidateUser.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE [dbo].[ValidateUser]  
-@email varchar(100),  
-@password varchar(20) 
-AS 
-BEGIN  
-SELECT [Id],[Email],[FirstName],[LastName] ,[Roles]  
-FROM [dbo].[User]  
-WHERE [Email]=@email AND [Password]=@password 
-END
